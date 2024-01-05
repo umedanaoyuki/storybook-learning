46b0dfba655bc662b91d2d609fc7b515bcf6f8d2
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-05 23:49:08 +0900

    MyButtonのファイル作成

---
+++ src/MyButton.js
@@ -0,0 +1,18 @@
+import './stories/button.css';
+
+export default function MyButton ({
+                           primary = false,
+                           size = 'medium',
+                           backgroundColor = null,
+                           label = 'Button',
+                           ...props
+                       }) {
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return (
+        <button type="button" className={['storybook-button', `storybook-button--${size}`, mode].join(' ')} style={{ backgroundColor }}
+    {...props}
+>
+    {label}
+    </button>
+);
+};
